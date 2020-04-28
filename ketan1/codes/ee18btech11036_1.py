6f6e941695f0c5ddc3464f65403e695ecceb8ec8
Piyush Kumar Uttam <43598226+piyushSTK@users.noreply.github.com>
2020-04-28 17:51:41 +0530

    Update ee18btech11036_1.py

--- ketan1/codes/ee18btech11036_1.py
+++ ketan1/codes/ee18btech11036_1.py
@@ -11,7 +11,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import control
 from scipy import signal
-
+#if using termux
+import subprocess
+import shlex
+#end if
 
 k=96
 num=[k]
@@ -39,14 +42,20 @@ plt.xlabel("Frequency")
 plt.grid()
 
 
-# plt.subplot(2,1,2)
-# plt.semilogx(w, phase)
-# plt.plot(w, phase_y)
-# plt.plot(wp,-180+pm,'ro')
-# plt.plot(wp,-180,'ro')
-# plt.text(wp ,-180+pm,'({}, {})'.format(wp, -180+pm))
-# plt.text(wp ,-180,'({}, {})'.format(wp, -180))
-# plt.ylabel("Phase")
-# plt.xlabel("Frequency")
-# plt.grid()
-# plt.show()
+plt.subplot(2,1,2)
+plt.semilogx(w, phase)
+plt.plot(w, phase_y)
+plt.plot(wp,-180+pm,'ro')
+plt.plot(wp,-180,'ro')
+plt.text(wp ,-180+pm,'({}, {})'.format(wp, -180+pm))
+plt.text(wp ,-180,'({}, {})'.format(wp, -180))
+plt.ylabel("Phase")
+plt.xlabel("Frequency")
+plt.grid()
+
+#if using termux
+plt.savefig('./figs/ee18btech11001/ee18btech11001_2.pdf')
+plt.savefig('./figs/ee18btech11001/ee18btech11001_2.eps')
+subprocess.run(shlex.split("termux-open ./figs/ee18btech11001/ee18btech11001_2.pdf"))
+#else
+#plt.show()
